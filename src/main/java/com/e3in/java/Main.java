b94a45fff3f1129956d6088037771bf327df49a7
Nicolas <nicolas.droesch@gmail.com>
2024-03-17 19:52:38 +0100

    Ajout de l'icon

--- src/main/java/com/e3in/java/Main.java
+++ src/main/java/com/e3in/java/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -16,8 +17,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/MainView.fxml")));
         Scene scene = new Scene(root, 1100, 500);
-
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icon.png")));
         primaryStage.setTitle("Java FX Bibliothèque");
+        primaryStage.getIcons().add(icon);
         primaryStage.setScene(scene);
         primaryStage.show();
     }

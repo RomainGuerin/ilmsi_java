5ffb6623b0eee2339972030a3ccb438b3690c39b
Romain Guerin <romainguerin911@gmail.com>
2024-06-25 22:25:57 +0200

    TEST JAR and JAVADOC

--- src/main/java/com/e3in/java/App.java
+++ src/main/java/com/e3in/java/App.java
@@ -1,14 +1,6 @@
 package com.e3in.java;
 
-/**
- * Classe principale de l'application.
- * Cette classe sert de point d'entrée pour l'exécution de l'application.
- */
 public class App {
-    /**
-     * Méthode principale (point d'entrée) de l'application.
-     * @param args Les arguments de ligne de commande.
-     */
     public static void main(String[] args) {
         Main.main(args);
     }

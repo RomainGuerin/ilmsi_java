3a92113373a00ddef61f189ce0322896d3db77cd
yraza782 <yoan78200@gmail.com>
2024-06-25 14:35:53 +0200

    edit javadoc

--- src/main/java/com/e3in/java/App.java
+++ src/main/java/com/e3in/java/App.java
@@ -1,6 +1,14 @@
 package com.e3in.java;
 
+/**
+ * Classe principale de l'application.
+ * Cette classe sert de point d'entrée pour l'exécution de l'application.
+ */
 public class App {
+    /**
+     * Méthode principale (point d'entrée) de l'application.
+     * @param args Les arguments de ligne de commande.
+     */
     public static void main(String[] args) {
         Main.main(args);
     }
